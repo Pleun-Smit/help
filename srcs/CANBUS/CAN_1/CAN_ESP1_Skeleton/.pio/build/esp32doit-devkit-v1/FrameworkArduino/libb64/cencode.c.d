6198a04bd6c3b190131c41d1257489dba6ae9e29
Sha,Ian C. <i.sha@student.fontys.nl>
2025-11-22 19:18:47 +0000

    Deleted this one because there wasnt any code in it

--- srcs/CANBUS/CAN_1/CAN_ESP1_Skeleton/.pio/build/esp32doit-devkit-v1/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/iansh/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/iansh/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h

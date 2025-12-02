df8f66dcfd1c4da061fe455fe3313f1ecc44b774
Pleun <pleun.smit@student.fontys.nl>
2025-12-02 15:53:09 +0100

    Updated MQTT, part of sync works, more modular functions added

--- srcs/MQTTTalkingSkeleton/MQTTtalkingskeleton/src/Refactor/MQTTHandler.cpp
+++ srcs/MQTTTalkingSkeleton/MQTTtalkingskeleton/src/Refactor/MQTTHandler.cpp
@@ -2,11 +2,11 @@
 
 MQTTHandler* MQTTHandler::instance = nullptr;
 
-MQTTHandler::MQTTHandler(const char* ssid, const char* password,
-                        const char* mqtt_server, std::shared_ptr<StationState> state) : client(wifi), ssid(ssid), password(password), mqtt_server(mqtt_server), state(state) 
-                        {
-                            instance = this;
-                        }
+MQTTHandler::MQTTHandler(const char* ssid, const char* password, const char* mqtt_server,
+                         std::shared_ptr<StationState> state, SystemState* systemState)
+    : client(wifi), ssid(ssid), password(password), mqtt_server(mqtt_server), state(state), systemState(systemState) {
+    instance = this;
+}
 
 void MQTTHandler::begin() {
     WiFi.begin(ssid, password);
@@ -20,44 +20,86 @@ void MQTTHandler::callbackThunk(char* topic, byte* payload, unsigned int length)
     if (instance) instance->callback(topic, payload, length);
 }
 
+int MQTTHandler::extractIdFromTopic(const String& topic) {
+    int firstSlash = topic.indexOf('/') + 1;
+    int secondSlash = topic.indexOf('/', firstSlash);
+    return topic.substring(firstSlash, secondSlash).toInt();
+}
+
 void MQTTHandler::callback(char* topic, byte* payload, unsigned int length) {
     String t(topic);
-    String expected = "station/" + String(state->id) + "/mode";
+    String msg;
+    for (int i = 0; i < length; i++) msg += (char)payload[i];
 
-    if (t == expected) {
-        String msg;
-        for (int i = 0; i < length; i++) msg += (char)payload[i];
+    if (t.startsWith("station/") && (t.endsWith("/status") || t.endsWith("/connection"))) {
+        int id = extractIdFromTopic(t);
+        StaticJsonDocument<200> doc;
+        if (deserializeJson(doc, msg)) return;
+
+        bool alive = doc["alive"];
+        String mode = doc["mode"] | "STATIC";
+        int power = doc["power"] | 0;
+
+        if (alive) {
+            systemState->updateStation(id, mode, power);
+        }
+    } else if (t == "system/mode") {
         state->setMode(msg);
     }
-    Serial.println(t);
 }
 
 void MQTTHandler::reconnect() {
     while (!client.connected()) {
         if (client.connect(("Station" + String(state->id)).c_str())) {
-            String topic = "station/" + String(state->id) + "/mode";
-            client.subscribe(topic.c_str());
+            client.subscribe("station/+/status");
+            client.subscribe("station/+/connection");
+            client.subscribe("system/mode");
         } else {
             delay(3000);
         }
     }
 }
 
-void MQTTHandler::publishStatus() {
+void MQTTHandler::publishStatusToDashboard() {
     static unsigned long lastPublish = 0;
     if (millis() - lastPublish > 2000) {
         lastPublish = millis();
-        String payload = "{\"alive\":true,\"power\":"
-                         + String(state->power) +
-                         ",\"mode\":\"" + state->mode + "\"}";
+        String payload = "{\"alive\":true,\"power\":" + String(state->power) + ",\"mode\":\"" + state->mode + "\"}";
         String topic = "station/" + String(state->id) + "/status";
-        client.publish(topic.c_str(), payload.c_str());
-        Serial.println(payload);
+        client.publish(topic.c_str(), payload.c_str(), true);
+    }
+}
+
+void MQTTHandler::publishConnectionStatus() {
+    static unsigned long lastPublish = 0;
+    if (millis() - lastPublish > 1000) {
+        lastPublish = millis();
+        String payload = "{\"alive\":true,\"mode\":\"" + state->mode + "\"}";
+        String topic = "station/" + String(state->id) + "/connection";
+        client.publish(topic.c_str(), payload.c_str(), true);
     }
 }
 
 void MQTTHandler::update() {
     if (!client.connected()) reconnect();
     client.loop();
-    publishStatus();
+    publishStatusToDashboard();
+    publishConnectionStatus();
+    systemState->checkTimeouts();
+}
+
+void MQTTHandler::printData(){
+    // probleem voor later
+}
+
+void MQTTHandler::publish(String topic, String payload){
+        client.publish(topic.c_str(), payload.c_str(), true);
+}
+
+void MQTTHandler::publish(String topic, String payload, int interval){
+    static unsigned long lastPublish = 0;
+    if (millis() - lastPublish > interval) {
+        lastPublish = millis();
+        client.publish(topic.c_str(), payload.c_str(), true);
+    }
 }
--- srcs/MQTTTalkingSkeleton/MQTTtalkingskeleton/src/Refactor/MQTTHandler.h
+++ srcs/MQTTTalkingSkeleton/MQTTtalkingskeleton/src/Refactor/MQTTHandler.h
@@ -1,28 +1,38 @@
 #pragma once
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include "StationState.h"
+#include "SystemState.h"
 
-class MQTTHandler{
-    public:
-    MQTTHandler(const char* ssid, const char* password, const char* mqtt_server, std::shared_ptr<StationState>state);
-
+class MQTTHandler {
+public:
+    MQTTHandler(const char* ssid, const char* password, const char* mqtt_server,
+                std::shared_ptr<StationState> state, SystemState* systemState);
     void begin();
     void update();
 
-    private:
+
+private:
     WiFiClient wifi;
     PubSubClient client;
     std::shared_ptr<StationState> state;
+    SystemState* systemState;
 
     const char* ssid;
     const char* password;
     const char* mqtt_server;
 
+    void printData();
+    
+    void publish(String topic, String payload);
+    void publish(String topic, String payload, int interval);
     void reconnect();
-    void publishStatus();
+    void publishStatusToDashboard();
+    void publishConnectionStatus();
     static void callbackThunk(char*, byte*, unsigned int);
     void callback(char* topic, byte* payload, unsigned int length);
+    int extractIdFromTopic(const String& topic);
 
     static MQTTHandler* instance;
 };

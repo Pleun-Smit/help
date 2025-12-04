ffd59145535b3445ec6ec4ab5489f794e006ee5d
Ian Sha <i529968@fontysict.nl>
2025-12-04 15:09:56 +0100

    fixed the CAN receive problem, changed pointers and added CanBusInit() in the main.cpp file

---
+++ srcs/CANBUS/CAN_1/CAN_ESP1_Skeleton/src/CANCommunication.cpp
@@ -0,0 +1,87 @@
+#include "CANCommunication.h"
+
+CANCommunication::CANCommunication(unsigned long TRANSMIT_ID, unsigned long RECEIVE_ID)
+{
+    this->CANBusObjectPointer = new MCP_CAN(5);
+    this->TRANSMIT_ID = TRANSMIT_ID;
+    this->RECEIVE_ID = RECEIVE_ID;
+};
+
+void CANCommunication::CANBusInit()
+{
+    SPI.begin(ESP_CLK_PIN, ESP_MISO_PIN, ESP_MOSI_PIN, CAN_CS_PIN);
+
+  if (CANBusObjectPointer->begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
+  {
+    Serial.println("MCP2515 Initialized Successfully!");
+  }
+  else
+  {
+    Serial.println("Error Initializing MCP2515...");
+  }
+
+  CANBusObjectPointer->setMode(MCP_NORMAL);
+}
+
+void CANCommunication::CANBusSendMsg()
+{
+    if (millis() - lastSendTime >= 5000)
+    {
+        byte randomNumber = random(1,41); // generate test value
+        byte sndStat = CANBusObjectPointer->sendMsgBuf(TRANSMIT_ID, 0, 1, &randomNumber);
+
+        if (sndStat == CAN_OK)
+        {
+            // -------------------------------
+            // CHECK FOR ACK ERROR HERE               This doesnt really work with our transceiver, because this model doesnt need an ACK to send messages. 
+            // -------------------------------
+            byte errorAck = CANBusObjectPointer->getError();
+
+            if (errorAck != 0)
+            {
+                Serial.print("Sent Number: ");
+                Serial.print(randomNumber);
+                Serial.print("  (ID: 0x");
+                Serial.print(TRANSMIT_ID, HEX);
+                Serial.println(")");
+
+                Serial.print("⚠️ ERROR: No ACK on CAN bus!  Error Flags = 0x");
+                Serial.println(errorAck, HEX);
+            }
+            else
+            {
+                Serial.print("Sent Number: ");
+                Serial.print(randomNumber);
+                Serial.print("  (ID: 0x");
+                Serial.print(TRANSMIT_ID, HEX);
+                Serial.println(")");
+            }
+        }
+        else
+        {
+            Serial.println("❌ Error sending CAN message...");
+        }
+
+        lastSendTime = millis();
+    }
+}
+
+void CANCommunication::CANBusReceiveMsg(byte* receivedNumber)
+{
+    if (!digitalRead(CAN_INT))
+    {
+        CANBusObjectPointer->readMsgBuf(&rxId, &len, rxBuf);
+
+        if (rxId == RECEIVE_ID && len == 1)
+        {
+            *receivedNumber = rxBuf[0];
+
+            Serial.print("Received number: ");
+            Serial.print(*receivedNumber);
+            Serial.print("  (ID: 0x");
+            Serial.print(rxId, HEX);
+            Serial.println(")");
+
+        }
+    }
+}
--- srcs/CANBUS/CAN_1/CAN_ESP1_Skeleton/src/main.cpp
+++ srcs/CANBUS/CAN_1/CAN_ESP1_Skeleton/src/main.cpp
@@ -1,35 +1,16 @@
-#include <mcp_can.h>
-#include <SPI.h>
+#include "CANCommunication.h"
 
-#define CAN_CS 5
-#define CAN_INT 4
+#define TRANSMIT_ID 0x300
+#define RECEIVE_ID 0x200
 
-MCP_CAN CAN0(5);
-
-#define TRANSMIT_ID 0x200
-#define RECEIVE_ID 0x300
-
-long unsigned int rxId;
-unsigned char len = 0;
-unsigned char rxBuf[8];
-
-unsigned long lastSendTime = 0;
+CANCommunication* CanCom;
+byte receivedNumber;
 
 void setup()
 {
   Serial.begin(115200);
-  SPI.begin(18,19,23, CAN_CS);
-
-  if (CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
-  {
-    Serial.println("MCP2515 Initialized Successfully!");
-  }
-  else
-  {
-    Serial.println("Error Initializing MCP2515...");
-  }
-
-  CAN0.setMode(MCP_NORMAL);
+  CanCom = new CANCommunication(TRANSMIT_ID, RECEIVE_ID);
+  CanCom->CANBusInit();
 
   Serial.println("Node Ready!");
 }
@@ -40,66 +21,13 @@ void loop()
     // SEND A RANDOM NUMBER EVERY 5 SECONDS
     //--------------------------------------
 
-    if (millis() - lastSendTime >= 5000)
-    {
-        byte randomNumber = random(1,41);
-        byte sndStat = CAN0.sendMsgBuf(TRANSMIT_ID, 0, 1, &randomNumber);
-
-        if (sndStat == CAN_OK)
-        {
-            // -------------------------------
-            // CHECK FOR ACK ERROR HERE               This doesnt really work with our transceiver, because this model doesnt need an ACK to send messages. 
-            // -------------------------------
-            byte errorAck = CAN0.getError();
-
-            if (errorAck != 0)
-            {
-                Serial.print("Sent Number: ");
-                Serial.print(randomNumber);
-                Serial.print("  (ID: 0x");
-                Serial.print(TRANSMIT_ID, HEX);
-                Serial.println(")");
-
-                Serial.print("⚠️ ERROR: No ACK on CAN bus!  Error Flags = 0x");
-                Serial.println(errorAck, HEX);
-            }
-            else
-            {
-                Serial.print("Sent Number (ACK RECEIVED!): ");
-                Serial.print(randomNumber);
-                Serial.print("  (ID: 0x");
-                Serial.print(TRANSMIT_ID, HEX);
-                Serial.println(")");
-            }
-        }
-        else
-        {
-            Serial.println("❌ Error sending CAN message...");
-        }
-
-        lastSendTime = millis();
-    }
+    CanCom->CANBusSendMsg();
 
     //--------------------------------------
     //          RECEIVE MESSAGES
     //--------------------------------------
 
-    if (!digitalRead(CAN_INT))
-    {
-        CAN0.readMsgBuf(&rxId, &len, rxBuf);
-
-        if (rxId == RECEIVE_ID && len == 1)
-        {
-            byte receivedNumber = rxBuf[0];
-
-            Serial.print("Received number: ");
-            Serial.print(receivedNumber);
-            Serial.print("  (ID: 0x");
-            Serial.print(rxId, HEX);
-            Serial.println(")");
-
-        }
-    }
+    CanCom->CANBusReceiveMsg(&receivedNumber);
 }
 
 
